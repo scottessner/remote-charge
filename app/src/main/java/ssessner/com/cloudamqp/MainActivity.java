b006b32570df6b07978987e1f95163bc90686b97
scottessner <scott.essner@gmail.com>
2016-05-03 00:46:24 -0500

    Collecting data from charger and parsing to main activity.

--- app/src/main/java/ssessner/com/cloudamqp/MainActivity.java
+++ app/src/main/java/ssessner/com/cloudamqp/MainActivity.java
@@ -6,11 +6,16 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.util.Log;
 
@@ -34,10 +39,49 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void handleMessage(Message msg) {
                 String message = msg.getData().getString("msg");
-                TextView tv = (TextView) findViewById(R.id.messagesView);
+                try {
+                    JSONObject jsonMessage = new JSONObject(message);
+
+                    JSONArray voltages = jsonMessage.getJSONArray("cell_voltages");
+
+                    ProgressBar v1Progress = (ProgressBar) findViewById(R.id.v1progressBar);
+                    TextView v1View = (TextView)findViewById(R.id.v1View);
+                    double cell_1_voltage = voltages.getDouble(0);
+                    int cell_1_percent = (int)(100*(cell_1_voltage - 3.2));
+
+                    v1Progress.setProgress(cell_1_percent);
+                    v1View.setText(String.format("%1$.3f", cell_1_voltage) + " V");
+
+                    ProgressBar v2Progress = (ProgressBar) findViewById(R.id.v2progressBar);
+                    TextView v2View = (TextView)findViewById(R.id.v2View);
+                    double cell_2_voltage = voltages.getDouble(1);
+                    int cell_2_percent = (int)(100*(cell_2_voltage - 3.2));
+
+                    v2Progress.setProgress(cell_2_percent);
+                    v2View.setText(String.format("%1$.3f", cell_2_voltage) + " V");
+
+                    ProgressBar v3Progress = (ProgressBar) findViewById(R.id.v3progressBar);
+                    TextView v3View = (TextView)findViewById(R.id.v3View);
+                    double cell_3_voltage = voltages.getDouble(2);
+                    int cell_3_percent = (int)(100*(cell_3_voltage - 3.2));
+
+                    v3Progress.setProgress(cell_3_percent);
+                    v3View.setText(String.format("%1$.3f", cell_3_voltage) + " V");
+
+                    TextView current = (TextView) findViewById(R.id.currentView);
+                    current.setText(jsonMessage.getString("charge_current") + " mA");
+
+                    TextView modeView = (TextView) findViewById(R.id.statusView);
+                    modeView.setText(jsonMessage.getString("mode"));
+
+                }catch (JSONException ex){
+                    ex.printStackTrace();
+                }
+
+
                 Date now = new Date();
                 SimpleDateFormat ft = new SimpleDateFormat ("hh:mm:ss");
-                tv.append(ft.format(now) + ' ' + message + '\n');
+                //tv.setText(ft.format(now) + ' ' + message + '\n');
             }
         };
         subscribe(incomingMessageHandler);
